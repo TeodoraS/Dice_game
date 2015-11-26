bc1a4d14982bd713a7c6d470de7d145d754d0e72
TeodoraS <tsoroiu@gmail.com>
2015-11-26 12:44:06 +0200

    Parsing arguments

--- dice-roll.py
+++ dice-roll.py
@@ -1,49 +1,73 @@
 import random
 import sys
+from argparse import ArgumentParser
+import argparse
 
-#It can be used with or without arguments. If used with arguments, first write the number of games you want to play, 
-#and second the number to be accumulated.
 
 all_rolls = []
-if len(sys.argv) > 1:
-    game_nr = int(sys.argv[1]) 
-else:
-    game_nr = int(raw_input("Insert the number of games:\t"))
 
-if len(sys.argv) > 1:
-    accum_nr = int(sys.argv[2])
-else:
-    accum_nr = int(raw_input("Insert the number to accumulate:\t"))
+def parse_opts():
+    parser = argparse.ArgumentParser(description = '''
+Dice-roll game, tracking frequency of rolls to accumulate 'n' points''')
 
-def roll_dice(accum_nr):
+    parser.add_argument('-g', '--games-number',
+                        help = '''Insert the number of games to be played.
+''',
+                        metavar = 'games-number',
+                        dest = 'game_nr',
+                        type = int
+                        )
+    parser.add_argument('-a', '--accumulate-number',
+                        help = '''Insert the number to be accumulated.
+''',
+                        metavar = 'accumulate-number',
+                        dest = 'accum_nr',
+                        type = int,
+                        )
+    return parser.parse_args()
+
+
+def roll_dice(accumulate):
     rolls =[]
-    while accum_nr > 0:
+    while accumulate > 0:
         roll = random.randrange(1, 7, 1)
-        accum_nr = accum_nr - roll
+        accumulate = accumulate - roll
         rolls.append(roll)
     all_rolls.append(rolls)
+    return all_rolls
 
-def verify_dice():
+
+def verify_dice(accumulate):
     for element in all_rolls:
-        if sum(element) != accum_nr:
+        if sum(element) != accumulate:
             all_rolls.remove(element)
         else:
             pass
+
         
-def sort_dice(accum_nr):
-    for i in range(1, (accum_nr+1)):
+def sort_dice(accumulate):
+    for i in range(1, (accumulate+1)):
         times = []
         for element in all_rolls:
             if len(element) == i:
                 times.append(element)
         print "Roll the dice %d times = %s games" % (i, len(times))
 
-while len(all_rolls) < game_nr :
-    roll_dice(accum_nr)
-    verify_dice()
-print all_rolls
-sort_dice(accum_nr)
 
-            
-
+def main():
+    arg = parse_opts()
+    if (arg.game_nr) and (arg.accum_nr) != None:
+        while len(all_rolls) < (arg.game_nr):
+            roll_dice(arg.accum_nr)
+            verify_dice(arg.accum_nr)
+        print all_rolls
+        sort_dice(arg.accum_nr)
+    else:
+        print "No arguments, please see -help."
     
+                
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception, e:
+        print e
